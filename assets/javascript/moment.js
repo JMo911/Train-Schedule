c5ae2ec0d1ae1fd26e5b9c3a86f5ff27371d2342
Jacob Moss <jacobmoss09@gmail.com>
2019-07-02 22:10:23 -0500

    database connection is up and running

---
+++ assets/javascript/moment.js
@@ -0,0 +1,33 @@
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+/* <script src="https://www.gstatic.com/firebasejs/6.2.4/firebase-app.js"> */
+
+// </script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#config-web-app -->
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCUCo7xCZxqgQykWxlqzkvnIX_K1X3WtFk",
+    authDomain: "the-mo-tenn-express.firebaseapp.com",
+    databaseURL: "https://the-mo-tenn-express.firebaseio.com",
+    projectId: "the-mo-tenn-express",
+    storageBucket: "",
+    messagingSenderId: "802940247853",
+    appId: "1:802940247853:web:51710a4c6bd4afe3"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+var mydatabase = firebase.database();
+
+
+// database.ref().on("value", function(snapshot){
+//     console.log(snapshot.val());
+// });
+
+
+
+
+    
