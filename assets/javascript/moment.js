4ef78a6d66a3931c37391d19f155886731c95e61
Jacob Moss <jacobmoss09@gmail.com>
2019-07-10 07:35:46 -0500

    closing for the day 6.9.19

--- assets/javascript/moment.js
+++ assets/javascript/moment.js
@@ -22,30 +22,94 @@
 
 var mydatabase = firebase.database();
 
+$("#submit").on("click", function(event){
+event.preventDefault();
 
+var trainName=$("#trainInput1").val().trim();
+var destination=$("#destination1").val().trim();
+var firstTrainTime=moment().format($("#trainTime1").val().trim(), "HH:mm");
+var frequency=$("#frequency1").val().trim();
+var possibleTrainTimes = [];
 
-mydatabase.ref().set({
-  "Trains": 
-  ["Motown", "Uptown", "Downtown", 
-  "Central Expressway", "AngieBus", 
-  "Belleville", "Bakerloo", "Piccadilly"],
+// console.log(frequency);
+// console.log(firstTrainTime);
 
-  "Destinations": 
-  ["Austin, TX", "Austin, TX", 
-  "Austin, TX", "Austin, TX", 
-  "Austin, TX", "Paris, France", 
-  "London, England", "London, England"],
-
-  "Firsttraintimes": 
-  ["0500", '1000', '0700', '0830', 
-  '1100', '0500', '0600', '0700'],
-
-  "Frequency": 
-  ['30', '20', '40', '15', 
-  '30', '60', '45', '15']
-
-});
+  // var runsperhour = 60/frequency;
+  // var runsperday = runsperhour*24;
+  // var m = moment(firstTrainTime);
+  // for (i = 0 ; i < runsperday ; i++) {
     
-// mydatabase.ref().on("value", funtion(snapshot){
+  //   console.log(m);
+  //   console.log(frequency);
+  //   var duration = moment.duration(frequency, "minutes");
+  //   console.log(duration);
 
-// })
+  //   // var times = moment(firstTrainTime).add((i*frequency), 'minutes');
+  //   // console.log(m.add(i*frequency, 'minutes'));
+  //   // console.log(i*frequency);
+  //   // possibleTrainTimes.push(times);
+  // }
+
+
+var newTrain = {
+  trainName: trainName,
+  destination: destination,
+  firstTrainTime: firstTrainTime,
+  frequency: frequency,
+  possibleTrainTimes: possibleTrainTimes
+};
+
+mydatabase.ref().push(newTrain);
+// console.log(newTrain.trainName);
+alert("Train added");
+
+$("#trainInput1").val("");
+$("#destination1").val("");
+$("#trainTime1").val("");
+$("#frequency1").val("");
+
+
+})
+
+mydatabase.ref().on("child_added", function(childSnapshot){
+  // console.log(childSnapshot.val());
+
+  var dataTrainName = childSnapshot.val().trainName;
+  var dataTrainDestination = childSnapshot.val().destination;
+  var dataFrequency = childSnapshot.val().frequency;
+  //next arrival time -- look at array of arrival times, compare to present time
+  //grab the next arrival time in the future
+  var firstTrainTime = childSnapshot.val().firstTrainTime; 
+  //format first train time with momentJS
+  var firstTrainTimeFormatted = moment().format(firstTrainTime, "HH:mm");
+
+
+  var timeDifference = moment().diff(moment(firstTrainTimeFormatted), "minutes");
+  var remainingTime = timeDifference % childSnapshot.val().frequency;
+  var minAway = childSnapshot.val().frequency - remainingTime;
+  var nextArrival = moment().add(minAway, "minutes");
+
+
+  //subtract next arrival time in the future from the present moment
+  
+
+
+  // console.log(firstTrainTimeFormatted);
+  var diffbetweenpresentandfirsttrain = moment().diff(moment(firstTrainTimeFormatted, "HH:mm"), "minutes");
+// console.log(diffbetweenpresentandfirsttrain);
+
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(dataTrainName),
+    $("<td>").text(dataTrainDestination),
+    $("<td>").text(dataFrequency),
+    //next arrival time
+    // $("<td>").text(moment(nextArrival).format("HH:mm")),
+    //min away
+    // $("<td>").text(minAway)
+  );
+
+  $("#trainTable > tbody").append(newRow);
+
+
+})
